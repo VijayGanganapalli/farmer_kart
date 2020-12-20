03bbe3c2872e65769d9f01fa6cefb0ed4e723a36
vijay <vijayganganapalli@gmail.com>
2020-12-20 20:42:44 +0530

    added login screen to app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,11 @@
+import 'package:farmer_kart/src/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: LoginScreen(),
+    );
   }
 }
