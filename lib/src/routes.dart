b4d0eefc61cec4ec424416009878de52c0d9ec24
vijay <vijayganganapalli@gmail.com>
2020-12-20 22:01:25 +0530

    added cupertino routes to app

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,6 +1,7 @@
 import 'package:farmer_kart/src/screens/landing_screen.dart';
 import 'package:farmer_kart/src/screens/login_screen.dart';
 import 'package:farmer_kart/src/screens/signup_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 abstract class Routes {
@@ -17,6 +18,23 @@ abstract class Routes {
         break;
       default:
         return MaterialPageRoute(builder: (context) => LoginScreen());
+        break;
+    }
+  }
+
+  static CupertinoPageRoute cupertinoPageRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return CupertinoPageRoute(builder: (context) => LandingScreen());
+        break;
+      case "/signup":
+        return CupertinoPageRoute(builder: (context) => SignupScreen());
+        break;
+      case "/login":
+        return CupertinoPageRoute(builder: (context) => LoginScreen());
+        break;
+      default:
+        return CupertinoPageRoute(builder: (context) => LoginScreen());
         break;
     }
   }
