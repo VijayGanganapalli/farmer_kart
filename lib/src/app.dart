dc49ec0543f4f9f2e0f9d1c97a6c8629d19501ad
vijay <vijayganganapalli@gmail.com>
2020-12-20 22:02:23 +0530

    added platform specific to app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,30 @@
+import 'dart:io';
+
 import 'package:farmer_kart/src/routes.dart';
 import 'package:farmer_kart/src/screens/login_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-      onGenerateRoute: Routes.materialPageRoute,
-    );
+    return PlatformApp();
+  }
+}
+
+class PlatformApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoApp(
+        home: LoginScreen(),
+        onGenerateRoute: Routes.cupertinoPageRoute,
+      );
+    } else {
+      return MaterialApp(
+        home: LoginScreen(),
+        onGenerateRoute: Routes.materialPageRoute,
+      );
+    }
   }
 }
